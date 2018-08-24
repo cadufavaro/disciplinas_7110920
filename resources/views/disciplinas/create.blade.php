6f376c3f279ae3f2dbe9ef6b4dd45a5b0d351f88
Cadu <cadufavaro@gmail.com>
2018-08-24 15:09:05 -0300

    Issue #4 resolvida

---
+++ resources/views/disciplinas/create.blade.php
@@ -0,0 +1,6 @@
+<form method="POST" action="/disciplinas">
+	{{ csrf_field() }}
+	Nome: <input name="titulo"><br><br>
+	Ementa: <textarea name="ementa"></textarea><br><br>
+	<button type="submit">Salvar</button>
+</form>
--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,3 +1,4 @@
+<a href="/disciplinas/create">Cadastrar Disciplina</a><br>
 <ul>
 	@foreach($disciplinas as $disciplina) 
 		<a href="/disciplinas/{{ $disciplina->id }}"><li>{{ $disciplina->titulo}}</li></a>
